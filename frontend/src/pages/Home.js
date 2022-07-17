2ce2ed40663081f8817f46e1f92a7b668ab4c1f3
Salila Herath <salilaherath@gmail.com>
2022-07-17 11:07:38 +0530

    Fetching data from the API

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import WorkoutDetails from '../components/WorkoutDetails';
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkout = async () => {
+      const response = await fetch('/api/workouts');
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkout();
+  }, []);
+
   return (
     <div className='home'>
-      <h2>Home</h2>
+      <div className='workouts'>
+        {workouts &&
+          workouts.map((workout) => <WorkoutDetails key={workout._id} workout={workout} />)}
+      </div>
     </div>
   );
 };
