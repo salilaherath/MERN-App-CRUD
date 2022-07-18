fa383fb8bab1933e428025315f49e08c8b490880
Salila Herath <salilaherath@gmail.com>
2022-07-18 18:21:50 +0530

    Created Add new workout form

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -22,6 +23,7 @@ const Home = () => {
         {workouts &&
           workouts.map((workout) => <WorkoutDetails key={workout._id} workout={workout} />)}
       </div>
+      <WorkoutForm />
     </div>
   );
 };
