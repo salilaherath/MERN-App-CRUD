36f56192cd455f374d71be646464b96181353547
Salila Herath <salilaherath@gmail.com>
2022-07-29 05:49:23 +0530

    React context for automatic update the page

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
 const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
   const [title, setTitle] = React.useState('');
   const [load, setLoad] = React.useState('');
   const [reps, setReps] = React.useState('');
@@ -29,6 +31,7 @@ const WorkoutForm = () => {
       setReps('');
       setError(null);
       console.log('new workout added', json);
+      dispatch({ type: 'CREATE_WORKOUT', payload: json });
     }
   };
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 import WorkoutDetails from '../components/WorkoutDetails';
 import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null);
+  const { workouts, dispatch } = useWorkoutsContext();
+
   useEffect(() => {
     const fetchWorkout = async () => {
       const response = await fetch('/api/workouts');
       const json = await response.json();
 
       if (response.ok) {
-        setWorkouts(json);
+        dispatch({ type: 'SET_WORKOUTS', payload: json });
       }
     };
     fetchWorkout();
